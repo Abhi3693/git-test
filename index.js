cab231f46e8fe96589fdf6f7a9067d9857148eb4
Abhijeet Mane <abhi@Varshas-MacBook-Air.local>
2022-08-19 14:07:35 +0530

    Added feature3

--- index.js
+++ index.js
@@ -8,8 +8,11 @@ function feature2() {
   return "feature2";
 }
 
+console.log(feature2("What are you doing"));
+
+
 function feature3() {
   return "feature3";
 }
 
-console.log(feature2());
+console.log(feature3("Did you have dinner?"));
